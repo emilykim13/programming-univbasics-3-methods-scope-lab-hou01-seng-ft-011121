86f4e012d9a3f6f949e879aeca01d742fb07c82b
Emily Nguyen <emilyn0614@gmail.com>
2020-12-30 15:25:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,11 +7,3 @@ def toadstool
   puts status
 end
 
-describe "all_phrases" do
-  it "puts out all of the previous catch phrases"
-  do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! ButOur Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end
-end
